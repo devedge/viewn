dae8569cd0bd7d08b84fc7634793c9b3a744da04
devedge <devedge@outlook.com>
2018-04-04 13:49:16 -0400

    Removed old project, added folder + basic static file server

---
+++ viewn-vidfs/README.md
@@ -0,0 +1,6 @@
+### `viewn-vidfs`
+  - Init with `express --view pug viewn-vidfs`
+  - Run with:
+    `DIR=/path/to/video/dir PORT=3001 npm start`
+    to specify a custom directory to serve content from port 3001
+  - Everything is served statically
--- viewn-vidfs/app.js
+++ viewn-vidfs/app.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/video');
-//var users = require('./routes/users');
-
 var app = express();
+
+// Determine the absolute path to the video directory from
+// the 'DIR' environment variable
+var videodir = path.resolve(process.env.DIR);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,12 +20,13 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/video', video);
-//app.use('/users', users);
+// If the DIR env was set, watch it
+if (videodir) {
+  app.use(express.static(videodir));
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
