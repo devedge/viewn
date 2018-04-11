e098d6ae29b961246e2b3cbb08cc77f81148063d
devedge <devedge@outlook.com>
2018-04-10 21:54:31 -0400

    Added webpack modules to compile scss

--- viewn-web/webpack.config.js
+++ viewn-web/webpack.config.js
@@ -9,14 +9,28 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'static', 'dist'),
     filename: '[name].bundle.js'
+    // Outputs are: (both under /dist/)
     // global.bundle.js
     // video.bundle.js
   },
   module: {
     rules: [
-      {
+      { // run for css files
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader"   // translates CSS into CommonJS
+        }]
+      }, { // run for scss files
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader"   // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader"  // compiles Sass to CSS
+        }]
       }
     ]
   },
